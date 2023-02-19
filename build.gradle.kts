a204e65aeb5fd7dd3797fa49ea08a69e563d775d
ruffrick <64497918+ruffrick@users.noreply.github.com>
2023-02-19 12:43:11 +0100

    Use jda-kotlinx

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,28 +11,26 @@ plugins {
 }
 
 group = "dev.ruffrick"
-version = "1.0"
+version = "0.1"
 
 repositories {
     mavenCentral()
     maven("https://m2.dv8tion.net/releases")
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
-    implementation(kotlin("stdlib"))
-
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-reflect
     implementation(kotlin("reflect"))
-
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-serialization-json
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
 
     // https://github.com/DV8FromTheWorld/JDA/
-    compileOnly("net.dv8tion:JDA:4.3.0_285")
+    compileOnly("net.dv8tion:JDA:4.3.0_287")
+
+    // https://github.com/ruffrick/jda-kotlinx
+    api("com.github.ruffrick:jda-kotlinx:c7cd4a4")
 
     // https://mvnrepository.com/artifact/org.reflections/reflections
     implementation("org.reflections:reflections:0.9.12")
@@ -42,14 +40,27 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+val sourcesJar = task<Jar>("sourcesJar") {
+    from(sourceSets["main"].allSource)
+    archiveClassifier.set("sources")
+}
+
+tasks {
+    build {
+        dependsOn(sourcesJar)
+        dependsOn(jar)
+    }
+}
+
 publishing {
     publications {
-        create<MavenPublication>("Release") {
+        create<MavenPublication>("release") {
             groupId = project.group as String
             artifactId = project.name
             version = project.version as String
 
             from(components["kotlin"])
+            artifact(sourcesJar)
         }
     }
 }
